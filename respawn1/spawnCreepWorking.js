192ec9efad56ae090f5f49cd65955f9abbb80359
KaliKartoffel <edthofermatthias@gmail.com>
2018-07-25 19:52:00 +0100

    bugfixin

--- respawn1/spawnCreepWorking.js
+++ respawn1/spawnCreepWorking.js
@@ -1,11 +1,20 @@
 var spawnCreepWorking = {
-    run: function(spawn, workStats, name, memory) { //memory = json object
-        Game.spawns[spawn].spawnCreep(workStats, name)
+    spawnQue: function(spawn, memory) { //memory = json object
+        Game.spawns[spawn].spawnCreep(memory["parts"], memory["role"] + Game.time)
         for (let key in memory) {
             if (memory.hasOwnProperty(key)) {
                 Game.creeps[name].key = memory[key];
             }
         }
+    }, 
+    newCreep: function(spawn, memory, workStats = memory["parts"], name = memory["role"] + Game.time) { //memory = json object
+        Game.spawns[spawn].spawnCreep(memory["parts"], memory["role"] + Game.time)
+        for (let key in memory) {
+            if (memory.hasOwnProperty(key)) {
+                Game.creeps[name].key = memory[key];
+            }
+        }
+        Game.creeps[name].memory["parts"] = workStats;
 	}
 };
 
--- respawn1/spawnCreeps.js
+++ respawn1/spawnCreeps.js
@@ -1,11 +1,15 @@
 var spawnCreepWorking = require('spawnCreepWorking')
 
 var spawnCreeps = {
-    run: function(creep) {
+    run: function() {
         if (Memory.SpawnQue.length) {
             creepMem = Memory.SpawnQue.length[0];
-            spawnCreepWorking.run("Spawn1", creepMem["parts"], creepMem["role"] + Game.time, creepMem);
+            spawnCreepWorking.spawnQue("Spawn1", creepMem);
+        } else { //spawnNewCreeps
+
         }
+
+
     }
 };
 
