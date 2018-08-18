6582505e019dd071751711ddc853fa0f96474658
KaliKartoffel <edthofermatthias@gmail.com>
2018-08-18 16:35:36 +0200

    a

--- respawn1/main.js
+++ respawn1/main.js
@@ -1,18 +1,3 @@
-var garbageCollector = require('garbageCollector');
-var spawnCreeps = require('spawnCreeps');
-
 module.exports.loop = function () {
-    garbageCollector.run();
-    spawnCreeps.run();
-
-    for (let i in Game.creeps) {
-        creep = Game.creeps[i];
-        if (creep.memory.role === "harvesterClose") {
-            var harvesterClose = require('harvesterClose');
-            harvesterClose.run(creep); 
-        } else if (creep.memory.role === "carry") {
-            var carry = require('carry');
-            carry.run(creep);
-        }
-    }
+    
 } 
