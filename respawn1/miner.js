32f4b6c3f2232c447d698663e46f121f0acec063
KaliKartoffel <edthofermatthias@gmail.com>
2018-08-19 15:58:07 +0200

    a

--- respawn1/miner.js
+++ respawn1/miner.js
@@ -1,5 +1,11 @@
 var miner = {
     run: function(creep) {
+        if (creep.memory.mining && creep.carry.energy == cappacity) {
+            creep.memory.mining = false;
+        } else if (!creep.memory.dispencing && creep.carry.energy == 0) {
+            creep.memory.dispencing = true;
+        }
+
         if (creep.memory.mining) {
             if(creep.harvest(Game.getObjectById(creep.memory.mySource)) == ERR_NOT_IN_RANGE) {
                 if (creep.moveByPath(Memory.sources[creep.memory.mySource]["pathToSource"]) == ERR_NOT_FOUND) {
