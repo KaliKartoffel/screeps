7e241c6ebb0b5bbdeb857c918da05fe36e9b17bf
KaliKartoffel <edthofermatthias@gmail.com>
2018-08-19 18:32:44 +0200

    a

--- respawn1/spawner.js
+++ respawn1/spawner.js
@@ -4,12 +4,12 @@ var spawner = {
     run: function (spawn) {
         //TODO: SpawnQue, just for the start:
         if (spawn.energy == 300) {
-            var transportCount = 0
+            var carryCoun = 0
             var minerCount = 0;
             for (let i in Game.creeps) {
-                switch (Game.creeps[i]) {
-                    case "transport":
-                        transportCount++;
+                switch (Game.creeps[i].memory.role) {
+                    case "carry":
+                        carryCoun++;
                         break;
                     case "miner":
                         minerCount++;
@@ -18,11 +18,10 @@ var spawner = {
                         break;
                 }
             }
-            console.log(minerCount, transportCount);
             if (minerCount < 1) {
-                spawnCreepWoking.run(spawn, [MOVE,WORK,WORK,CARRY], "test miner" + Game.time, {role: "miner", mySource: "59f1a24382100e1594f39ac2", mining: false});
-            } else if (transportCount < 1) {
-                spawnCreepWoking.run(spawn, [MOVE,CARRY,CARRY,CARRY], "test transport" + Game.time, {role: "transport", mySource: "59f1a24382100e1594f39ac2", dispercing: false});
+                spawnCreepWoking.run(spawn, [MOVE,WORK,WORK,CARRY], "test miner" + Game.time, {role: "miner", mySource: "59f1a24382100e1594f39ac2", mining: true});
+            } else if (carryCount < 1) {
+                spawnCreepWoking.run(spawn, [MOVE,CARRY,CARRY,CARRY], "test transport" + Game.time, {role: "carry", mySource: "59f1a24382100e1594f39ac2", dispercing: false});
             } else {
                 spawnCreepWoking.run(spawn, [MOVE,CARRY,WORK], "test upgrader" + Game.time, {role: "upgrader", mySource: "59f1a24382100e1594f39ac1", mining: false});
             }
