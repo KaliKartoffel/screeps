73e5857c589eab0ee82618e8fe6e75b22151e4bf
KaliKartoffel <edthofermatthias@gmail.com>
2018-08-19 18:34:50 +0200

    a

--- respawn1/upgrader.js
+++ respawn1/upgrader.js
@@ -1,11 +1,11 @@
 var roleUpgrader = {
     run: function(creep) {
-        if(creep.memory.mining && creep.carry.energy == 0) {
+        if(creep.memory.mining && creep.carry.energy == creep.carryCapacity) {
             creep.memory.mining = false;
-        } else if(!creep.memory.mining && creep.carry.energy == creep.carryCapacity) {
+        } else if(!creep.memory.mining && creep.carry.energy == 0) {
             creep.memory.mining = true;
         }
-	    if(mining) {
+	    if(creep.memory.mining) {
             if (creep.harvest(Game.getObjectById(creep.memory.mySource)) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(Game.getObjectById(creep.memory.mySource));
             }
